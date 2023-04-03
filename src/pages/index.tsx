1b02718400cd00c159e9c19bdae4b262c3533c7e
S Kota <91359399+Skota11@users.noreply.github.com>
2023-04-03 20:04:39 +0900

    fix_packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { useState , useEffect } from 'react';
 
 import Avatar from '@mui/material/Avatar';
 
@@ -11,8 +8,6 @@ import Avatar from '@mui/material/Avatar';
 import { client } from "../libs/cms_client";
 //components
 import Header from "../components/header";
-import { element } from 'svelte/internal';
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({contents} : {contents:[]}) {
   return (
